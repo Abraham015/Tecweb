5a7a76f7915ad820f92a9a33a80be0e262426e8f
Abraham015 <79383720+Abraham015@users.noreply.github.com>
2021-05-25 23:47:12 -0500

    Add files via upload

--- datos.php
+++ datos.php
@@ -28,26 +28,26 @@
 	$cel=$_REQUEST["cel"];
 	$correo=$_REQUEST["correo"];
 	switch ($_REQUEST["Procedencia"]) {
-		case '1': $procedencia="CECyT #1 Gonzalo Vázquez Vela"; break;
+		case '1': $procedencia="CECyT #1 Gonzalo Vazquez Vela"; break;
 		case '2': $procedencia="CECyT #2 Miguel Bernard"; break;
-		case '3': $procedencia="CECyT #3 Estanislao Ramírez Ruiz"; break;
-		case '4': $procedencia="CECyT #4 Lázaro Cardenas"; break;
-		case '5': $procedencia="CECyT #5 Benito Juárez García"; break;
-		case '6': $procedencia="CECyT #6 Miguel Ohtón de Mendizabal"; break;
-		case '7': $procedencia="CECyT #7 Cuauhtémoc"; break;
-		case '8': $procedencia="CECyT #8 Narciso Bassols García"; break;
-		case '9': $procedencia="CECyT #9 Juan de Dios Bátiz"; break;
-		case '10': $procedencia="CECyT #10 Carlos Vallejo Márquez"; break;
-		case '11': $procedencia="CECyT #11 Wilfrido Massieu Pérez"; break;
-		case '12': $procedencia="CECyT #12 José María Morelos y Pavón"; break;
-		case '13': $procedencia="CECyT #13 Ricardo Flores Magón"; break;
+		case '3': $procedencia="CECyT #3 Estanislao Ramirez Ruiz"; break;
+		case '4': $procedencia="CECyT #4 Lazaro Cardenas"; break;
+		case '5': $procedencia="CECyT #5 Benito Juarez Garcia"; break;
+		case '6': $procedencia="CECyT #6 Miguel Ohton de Mendizabal"; break;
+		case '7': $procedencia="CECyT #7 Cuauhtemoc"; break;
+		case '8': $procedencia='CECyT #8 Narciso Bassols Garcia'; break;
+		case '9': $procedencia="CECyT #9 Juan de Dios Batiz"; break;
+		case '10': $procedencia="CECyT #10 Carlos Vallejo Marquez"; break;
+		case '11': $procedencia="CECyT #11 Wilfrido Massieu Perez"; break;
+		case '12': $procedencia="CECyT #12 Jose Maria Morelos y Pavon"; break;
+		case '13': $procedencia="CECyT #13 Ricardo Flores Magon"; break;
 		case '14': $procedencia="CECyT #14 Luis Enrique Erro"; break;
-		case '15': $procedencia="CECyT #15 Diódoro Antúnez Echegaray"; break;
+		case '15': $procedencia="CECyT #15 Diodoro Antunez Echegaray"; break;
 		case '16': $procedencia="CECyT #16 Hidalgo"; break;
-		case '17': $procedencia="CECyT #17 León-Guanajuato"; break;
+		case '17': $procedencia="CECyT #17 Leon-Guanajuato"; break;
 		case '18': $procedencia="CECyT #18 Zacatecas"; break;
 		case 'C1': $procedencia="CET #1 Walter Cross Buchanan"; break;
-		default: $procedencia="Escuela externa"; break;
+		default: $procedencia=$_REQUEST["school"];
 	}
 	switch ($_REQUEST["Estado"]) {
 		case 'Agua': $estado="Aguascalientes"; 	break;
@@ -84,31 +84,36 @@
 		case 'Zac': $estado="Zacatecas"; break;
 		default: echo "Opción no valida"; break;
 	}
-	if ($_POST["school"]==""){ $escuela="";}else{$escuela=$_POST["school"];}
 	$promedio=$_REQUEST["promedio"];
 	switch ($_REQUEST["opcion"]) {
-		case 'primera':
-			$opcion="Primera Opción";
-		break;
-		case 'segunda':
-			$opcion="Segunda Opción";
-		break;
-		case 'tercera':
-			$opcion="Tercera Opción";
-		break;
-		case 'cuarta':
-			$opcion="Cuarta Opción";
-		break;
-		default:
-			echo "Escoge una opción";
-		break;
+		case 'primera': $opcion="Primera Opción"; break;
+		case 'segunda': $opcion="Segunda Opción"; break;
+		case 'tercera': $opcion="Tercera Opción"; break;
+		case 'cuarta': $opcion="Cuarta Opción"; break;
+		default: echo "Escoge una opción"; break;
 	}
 
-	$sql = "INSERT INTO identidad (Boleta,Nombre,AP,AM,fecha,Genero,CURP)
-	VALUES ($bol,'$nom','$apellido','$ape',$fe,'$gen','$curp')";
+	$identidad = "INSERT INTO identidad (Boleta,Nombre,AP,AM,fecha,Genero,CURP)
+	VALUES ($bol,'$nom','$apellido','$ape','$fe','$gen','$curp')";
 
-	if ($conn->query($sql) === TRUE) {
-	  echo "<script>alert('".$var."'');</script>";
+	$contacto="INSERT INTO Contacto (Calle,Colonia,CP,Celular,Correo)
+	VALUES ('$calle','$colonia',$CP,$cel,'$correo')";
+
+	$pro="INSERT INTO Procedencia (Escuela,Entidad,Promedio,opcion)
+	VALUES ('$procedencia','$estado',$promedio,'$opcion')";
+
+	if ($conn->query($identidad) === TRUE) {
+	  //echo "New record created successfully";
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	if ($conn->query($contacto) === TRUE) {
+	  //echo "New record created successfully";
+	} else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	if ($conn->query($pro) === TRUE) {
+	  echo "New record created successfully";
 	} else {
 	  echo "Error: " . $sql . "<br>" . $conn->error;
 	}
